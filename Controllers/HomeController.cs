e4f8036a2ea9e258604b69d046d8707967bd77e4
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-13 16:26:45 +0300

    add application components (model view controler) and change port

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers;
+
+public class HomeController:Controller
+{
+    public ViewResult Index()
+    {
+        return View(new string[] { "C#","Language","Features"});
+    }
+}
---
+++ Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LanguageFeatures.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
