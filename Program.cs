145d1075989eea7cc93e5076046616fbcf598663
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-13 16:10:01 +0300

    enable mvc

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+builder.Services.AddControllersWithViews();
+//app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 app.Run();
